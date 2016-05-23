eaedba6ef68eb6242706ab0062e1bf2af9fec926
rcpeters <info@rcpeters.com>
2016-05-23 02:08:32 -0700

    fix spec

--- spec/orcidSearchUrlSpec.js
+++ spec/orcidSearchUrlSpec.js
@@ -15,7 +15,7 @@ describe(
 						var url = orcidSearchUrlJs.buildUrl(input);
 						expect(url)
 								.toBe(
-										'https://orcid.org/v1.1/search/orcid-bio/?q=%7B!edismax%20qf%3D%22given-and-family-names%5E50.0%20family-name%5E10.0%20given-names%5E5.0%20credit-name%5E10.0%20other-names%5E5.0%20text%5E1.0%22%20pf%3D%22given-and-family-names%5E50.0%22%20mm%3D1%7Dmy%20search%20terms&start=0&rows=10');
+										'https://pub.orcid.org/v1.1/search/orcid-bio/?q=%7B!edismax%20qf%3D%22given-and-family-names%5E50.0%20family-name%5E10.0%20given-names%5E5.0%20credit-name%5E10.0%20other-names%5E5.0%20text%5E1.0%22%20pf%3D%22given-and-family-names%5E50.0%22%20mm%3D1%7Dmy%20search%20terms&start=0&rows=10');
 					});
 			it(
 					'should be possible to use a different offset',
@@ -28,7 +28,7 @@ describe(
 						var url = orcidSearchUrlJs.buildUrl(input);
 						expect(url)
 								.toBe(
-										'https://orcid.org/v1.1/search/orcid-bio/?q=%7B!edismax%20qf%3D%22given-and-family-names%5E50.0%20family-name%5E10.0%20given-names%5E5.0%20credit-name%5E10.0%20other-names%5E5.0%20text%5E1.0%22%20pf%3D%22given-and-family-names%5E50.0%22%20mm%3D1%7Dmy%20search%20terms&start=15&rows=5');
+										'https://pub.orcid.org/v1.1/search/orcid-bio/?q=%7B!edismax%20qf%3D%22given-and-family-names%5E50.0%20family-name%5E10.0%20given-names%5E5.0%20credit-name%5E10.0%20other-names%5E5.0%20text%5E1.0%22%20pf%3D%22given-and-family-names%5E50.0%22%20mm%3D1%7Dmy%20search%20terms&start=15&rows=5');
 					});
 			it(
 					'should be possible to override the base URL',
@@ -52,7 +52,7 @@ describe('ORCID iD search', function() {
 			text : '0000-0003-4654-1403'
 		};
 		var url = orcidSearchUrlJs.buildUrl(input);
-		expect(url).toBe('https://orcid.org/v1.1/search/orcid-bio/?q=orcid:0000-0003-4654-1403&start=0&rows=10');
+		expect(url).toBe('https://pub.orcid.org/v1.1/search/orcid-bio/?q=orcid:0000-0003-4654-1403&start=0&rows=10');
 	});
 });
 
@@ -70,7 +70,7 @@ describe(
 						var url = orcidSearchUrlJs.buildUrl(input);
 						expect(url)
 								.toBe(
-										'https://orcid.org/v1.1/search/orcid-bio/?q=given-names%3Awill%20AND%20family-name%3Asimpson%20OR%20other-names%3Awill%20AND%20keyword%3Aphysics&start=0&rows=10');
+										'https://pub.orcid.org/v1.1/search/orcid-bio/?q=given-names%3Awill%20AND%20family-name%3Asimpson%20OR%20other-names%3Awill%20AND%20keyword%3Aphysics&start=0&rows=10');
 					});
 		});
 
